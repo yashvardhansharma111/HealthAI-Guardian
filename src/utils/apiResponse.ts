4c30214f97ca11e3a7d1b37257ea92fe7bc2b1fe
Atherban <work.with.atherban@gmail.com>
2025-11-28 21:04:28 +0530

    API response Fixed

--- src/utils/apiResponse.ts
+++ src/utils/apiResponse.ts
@@ -1,12 +1,16 @@
-import { NextResponse } from "next/server";
-
 export function success(data: any, status = 200) {
-  return NextResponse.json({ success: true, data }, { status });
+  return {
+    success: true,
+    status,
+    data,
+  };
 }
 
 export function failure(error: any, status = 400) {
-  return NextResponse.json(
-    { success: false, message: error.message || "Something went wrong" },
-    { status }
-  );
+  return {
+    success: false,
+    status,
+    message: error.message || "Something went wrong",
+    error,
+  };
 }
